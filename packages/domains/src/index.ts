3e478a6822b3c65403e22456969fca89ceb4c622
U <88958116+Apisit250aps@users.noreply.github.com>
2026-07-15 12:41:19 +0700

    feat(repository): add BaseUseCase and BaseRepository abstractions

---
+++ packages/domains/src/index.ts
@@ -0,0 +1,12 @@
+
+export abstract class BaseUseCase<Context, TOutput> {
+  abstract execute(context: Context): Promise<TOutput>;
+}
+
+export abstract class BaseRepository<T> {
+  abstract findAll(): Promise<T[]>;
+  abstract findById(id: string): Promise<T | null>;
+  abstract create(entity: T): Promise<void>;
+  abstract update(id: T): Promise<void>;
+  abstract delete(id: string): Promise<void>;
+}
---
+++ packages/domains/src/repositories/user.repo.ts
@@ -0,0 +1,8 @@
+import { BaseRepository } from "..";
+import { User } from "../entities/user";
+
+interface IUserRepository extends BaseRepository<User> {
+  findByEmail(email: string): Promise<User | null>;
+}
+
+export type { IUserRepository };
