7bf851f55fdea94234230fd6da85334e84eb3a49
U <88958116+Apisit250aps@users.noreply.github.com>
2026-07-15 08:37:27 +0700

    feat(auth): enable email and password authentication in betterAuth configuration

--- packages/infrastructures/src/auth/index.ts
+++ packages/infrastructures/src/auth/index.ts
@@ -4,5 +4,9 @@ import db from "@shop/database/db";
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
-  }),
+  },
+  ),
+  emailAndPassword: {
+    enabled: true,
+  },
 });
