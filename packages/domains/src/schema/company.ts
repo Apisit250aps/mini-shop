e9fb1f943f148d791663d1cc524ed8b29456d712
U <88958116+Apisit250aps@users.noreply.github.com>
2026-07-15 13:14:58 +0700

    feat(schema): add company and permissions schemas with related entities

---
+++ packages/domains/src/schema/company.ts
@@ -0,0 +1,44 @@
+import { BaseEntity, StringField, UUIDField } from "../lib/entity";
+import { z } from "zod";
+
+const companySchema = BaseEntity({
+  name: StringField({ required: true, }),
+  description: StringField({ required: true, }),
+  // relations
+  ownerId: UUIDField({ required: true, }),
+});
+
+const branchSchema = BaseEntity({
+  name: StringField({ required: true, }),
+  description: StringField({ required: true, }),
+  address: StringField({ required: true, }),
+  // relations
+  companyId: UUIDField({ required: true, }),
+});
+
+const departmentSchema = BaseEntity({
+  name: StringField({ required: true, }),
+  description: StringField({ required: true, }),
+  // relations
+  companyId: UUIDField({ required: true, }),
+});
+
+const userBranchSchema = BaseEntity({
+  userId: UUIDField({ required: true, }),
+  branchId: UUIDField({ required: true, }),
+});
+
+const userDepartmentSchema = BaseEntity({
+  userId: UUIDField({ required: true, }),
+  departmentId: UUIDField({ required: true, }),
+});
+
+
+type CompanyEntity = z.infer<typeof companySchema>;
+type BranchEntity = z.infer<typeof branchSchema>;
+type DepartmentEntity = z.infer<typeof departmentSchema>;
+type UserBranchEntity = z.infer<typeof userBranchSchema>;
+type UserDepartmentEntity = z.infer<typeof userDepartmentSchema>;
+
+export { companySchema, branchSchema, departmentSchema, userBranchSchema, userDepartmentSchema };
+export type { CompanyEntity, BranchEntity, DepartmentEntity, UserBranchEntity, UserDepartmentEntity };
